3ea34bbc5dc3f54b50903f0d4770e3ec3df6a908
hojihun5516 <hojihun5516@daum.net>
2021-12-07 01:11:56 +0000

    fix: To load pretrained model

--- generate.py
+++ generate.py
@@ -71,9 +71,10 @@ if __name__ == "__main__":
     g_ema = Generator(
         args.size, args.latent, args.n_mlp, channel_multiplier=args.channel_multiplier
     ).to(device)
-    checkpoint = torch.load(args.ckpt)
+    checkpoint = torch.load(args.ckpt, map_location=lambda storage, loc:storage)
 
-    g_ema.load_state_dict(checkpoint["g_ema"])
+    g_ema.load_state_dict(checkpoint["g_ema"], strict=False)
+    g_ema.eval()
 
     if args.truncation < 1:
         with torch.no_grad():
