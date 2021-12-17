cca44fe80b9a9575333821012756c848f880e27f
swkim-sm <swit1712155@sookmyung.ac.kr>
2021-12-17 06:20:06 +0000

    feat: Hide DB password using dotenv

--- sql_app/database.py
+++ sql_app/database.py
@@ -3,11 +3,17 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 from dotenv import load_dotenv
+import os
 
 load_dotenv(verbose=True)
 
-# database URL for SQLAlchemy
-MYSQL_DB = "mysql+pymysql://cv02:boostcampcv02@cv02.cufn2thqplnf.ap-northeast-2.rds.amazonaws.com:3306/cv02?charset=utf8"
+user = os.getenv("MYSQL_USER")
+password = os.getenv("MYSQL_PASSWORD")
+host = os.getenv("MYSQL_SERVER")
+port = os.getenv("MYSQL_PORT")
+database = os.getenv("MYSQL_DB")
+
+MYSQL_DB = f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}?charset=utf8"
 
 # Create the SQLAlchemy engine
 engine = create_engine(MYSQL_DB, encoding="utf-8")
