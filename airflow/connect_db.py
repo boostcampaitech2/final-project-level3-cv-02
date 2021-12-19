7d8345ddf477e9177e41a9dbd356d979d7e2eea6
sejongjeong <wjdtpwhd@gmail.com>
2021-12-19 08:58:03 +0000

    feat: load_data and save to csv

--- airflow/connect_db.py
+++ airflow/connect_db.py
@@ -1,5 +1,6 @@
 import pymysql
-import pandas as pd
+import csv
+import os
 
 class DBController:
 	def __init__(self, user='cv02', pw='boostcampcv02', host='cv02.cufn2thqplnf.ap-northeast-2.rds.amazonaws.com', db='cv02'):
@@ -10,22 +11,43 @@ class DBController:
 			db=db, 
 			charset='utf8'
 		)
-		self.cursor = self.connect.cursor(pymysql.cursors.DictCursor)
+		self.cursor = self.connect.cursor()
+		self.col_name = []
+		self.res = []
+
+	def createDirectory(self, directory = './csv'):
+		try:
+			if not os.path.exists(directory):
+				os.makedirs(directory)
+		except OSError:
+			print("Error: Failed to create the directory.")
+
 	
 	def load_data(self):
+		sql = 'SHOW FULL COLUMNS FROM inference_result;'
+		self.cursor.execute(sql)
+		rows = self.cursor.fetchall()
+		for i in range(len(rows)):
+			self.col_name.append(rows[i][0])
 		sql = 'SELECT * FROM inference_result;'
 		self.cursor.execute(sql)
-		res = cursor.fetchall()
+		rows = list(self.cursor.fetchall())
+		for i in range(len(rows)):
+			self.res.append(list(rows[i]))
 
-	def out_csv(self), result):
-		with open('mycsvfile.csv','w') as f:
+	def out_csv(self):
+		self.createDirectory()
+		with open('./csv/data.csv','w', newline='') as f:
 			w = csv.writer(f)
-			w.writerow(result.keys())
-			w.writerow(result.values())
+			w.writerow(self.col_name)
+			for i in range(len(self.res)):
+				w.writerow(self.res[i])
 
 	def save_data(self):
 		sql = ''
 		self.cursor.execute(sql)
-		res = cursor.fetchall()
+		col_name = cursor.fetchall()
 
-	
+# test = DBController()
+# test.load_data()
+# test.out_csv()
