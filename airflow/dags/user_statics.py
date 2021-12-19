20b7cfead5c30efeec95064da9a3bc2f9d802c71
Yiujin <ujinus@gmail.com>
2021-12-19 10:49:14 +0000

    feat : feature/age_gender_inference_batchjob (not completed)

--- airflow/dags/user_statics.py
+++ airflow/dags/user_statics.py
@@ -13,6 +13,8 @@ from airflow.models.variable import Variable
 import os
 import pymysql
 import pandas as pd
+from sqlalchemy import create_engine
+from collections import defaultdict
 
 # .env
 from dotenv import load_dotenv
@@ -44,22 +46,49 @@ def connect_db_get_data():
     select * from cv02.inference_result''', con = DB)
     print(data.shape[0])
 
-    os.mkdir(INFER_RESULT_PATH)
+    os.mkdir(INFER_RESULT_PATH, exist_ok=True)
     data.to_csv(f'{AIRFLOW_HOME}/infer_result/infer_result_latest.csv', index=False)
-    print(f'save inference_result_latest in {AIRFLOW_HOME}/infer_result')
+    print(f'save inference_result_latest in {INFER_RESULT_PATH}')
     return 
    
 
 def calculate_bounce_rate(result):
-    print(f'calculate bounce rate')
-    df = pd.read_csv(f'{AIRFLOW_HOME}/infer_result/infer_result_latest.csv')
-    
-    
+    print(f'calculate bounce rate after click inference button')
+    df_log = pd.read_csv(f'{AIRFLOW_HOME}/infer_result/infer_result_latest.csv')
+    df_statics = pd.DataFrame(columns = ['ID', 'age', 'gender', 'inference_bounce_rate'])
+
+    # calculate inference_bounce_rate
+    total_b_rate = len(df_log['compelete' == False]) / len(df_log) # inference_bounce_rate -> #bounce / #click_inference_button = #complete==False / #ID
+    df_statics['ID'] = [i for i in range(1,11)]
+
+    # gender - age 
+    gen_age = defaultdict(str)
+    age = [20,30,40,50]
+    gen_age['female'] = []
+    for a in age:
+        gen_age['female'].append(df_log['gender' == 'female' and 'age' < 20]) 
+
+
     return
 
 
-def data_to_db():
-    pass
+def data_to_db(INFER_RESULT_PATH):
+    MYSQL_USER_ID = Variable.get("MYSQL_USER_ID")
+    MYSQL_PASSWORD = Variable.get("MYSQL_PASSWORD")
+    MYSQL_PORT = Variable.get("MYSQL_PORT")
+    MYSQL_DBNAME = Variable.get("MYSQL_DBNAME")
+    MYSQL_SERVER = Variable.get("MYSQL_SERVER")
+
+    DB = pymysql.connect(host=MYSQL_SERVER, user=MYSQL_USER_ID, passwd=MYSQL_PASSWORD, port=int(MYSQL_PORT), charset='utf8')
+
+    infer_df = pd.read_csv(INFER_RESULT_PATH)
+
+    engine = create_engine()
+    engine_conn = engine.connect(f'mysql+pymysql://{MYSQL_USER_ID}:{MYSQL_PASSWORD}@{MYSQL_SERVER}:{MYSQL_PORT}/{MYSQL_DBNAME}?charset=utf8mb4')
+    infer_df.to_sql('inference_bounce_rate', engine_conn, if_exists='replace', index=None)
+    engine_conn.close()
+    engine.dispose()
+
 
 default_args = {
     'owner': 'ujin',
@@ -108,7 +137,13 @@ with DAG(
         dag=mysql_dag,
     )
 
-    get_infer_result_pymysql_task >> calculate_bounce_rate_task
+    data_to_db_task = PythonOperator(
+        task_id = 'calculate_bouce_rate',
+        python_callable=data_to_db,
+        dag=mysql_dag,
+    )
+
+    get_infer_result_pymysql_task >> calculate_bounce_rate_task >> data_to_db_task
 
 '''
 sql
