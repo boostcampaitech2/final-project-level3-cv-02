26466cbaedceba8a7a5d62f5b76035bd3a38fabc
swkim-sm <swit1712155@sookmyung.ac.kr>
2021-12-19 08:49:00 +0000

    feat: Connect DB

--- back-end/api/predictor.py
+++ back-end/api/predictor.py
@@ -3,7 +3,7 @@ from PIL import Image
 from typing import List
 import uuid
 import shutil
-from fastapi import APIRouter, File, UploadFile
+from fastapi import APIRouter, File, UploadFile, Depends, HTTPException
 from fastapi.responses import HTMLResponse, FileResponse
 from dotenv import load_dotenv
 from .s3 import *
@@ -11,9 +11,35 @@ from .uploader import upload_image
 import shutil 
 from babygan import inference_test
 
+from sqlalchemy.orm import Session
+
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+sys.path.append("../")
+
+from model import crud, models, schemas
+from model.database import SessionLocal, engine
+
 
 router = APIRouter()
 load_dotenv(dotenv_path = ".env")
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+# @router.get("/users/", response_model=List[schemas.InferenceResult])
+def read_queries(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    return crud.get_inference_results(db, skip=skip, limit=limit)
+
+# @router.post("/users",response_model=schemas.InferenceResult)
+def create_query(inference_result:schemas.InferenceResultCreate, db: Session = Depends(get_db)):
+    return crud.create_inference_result(db, inference_result=inference_result.dict())
 
 @router.post("/uploadfiles") # 추후에 uploadfiles 이름 변경 -> predict
 def predict(
@@ -28,21 +54,17 @@ def predict(
     Return:
         baby_url : S3에서 추출한 결과물 url을 반환합니다.
     """
-    # TODO: 부모 이미지 S3에 저장 -> uploader
     setting_uuid = str(uuid.uuid4())
-    father_url, mother_url = upload_image(setting_uuid, father_image, mother_image)
+    father_url = upload_image(setting_uuid, father_image, "father")
+    mother_url = upload_image(setting_uuid, mother_image, "mother")
     
-    # TODO: 찐 인퍼런스
-    ## 이 부분 저장하지 않고 바꾸는 걸로 수정해야 함
-    ## 기존 코드에서 엄마 아빠 순으로 넣고 있어서 수정
-    baby_file_path = inference_test.do_inference(father_url, mother_url, setting_uuid[:8])
+    # baby_file_path = inference_test.do_inference(father_url, mother_url, setting_uuid[:8])
 
-    # TODO: 벱비 이미지 S3에 저장
-    ## baby_file_path 제대로 전달되는지 확인 필요(file이 맞는지)
-    baby_url = upload_image(setting_uuid, baby_file_path)
+    # baby_url = upload_image(setting_uuid, baby_file_path, "baby")
 
-    # TODO: 벱비 url return
-    return { "baby_image_path": baby_url }
+    create_query(inference_result = ({"id":setting_uuid, "father_url":father_url, "mother_url":mother_url, "baby_url":"baby_url_test"}))
+
+    return { "baby_image_path": "baby_url" } # 요거 주석처리 한 것
 
 @router.get("/")
 def uploader():
--- back-end/api/uploader.py
+++ back-end/api/uploader.py
@@ -19,47 +19,28 @@ s3 = s3_connection (access_key_id, access_key_pass)
 
 def upload_image(
     common_uuid : str,
-    image1: File(...),
-    image2: Optional[str] = None
+    image: File(...),
+    file_name: str
     ):
     """
     image의 url을 추출하고 DB에 저장합니다.
-    image가 2개 들어오는 경우(부모 이미지 저장)와 1개 들어오는 경우(자식)를 커버합니다.
     Args:
         common_uuid : father, mother, baby의 공통 uuid
-        image1 : Father image if image2 exist else Baby image
-        image2 : Mother image if exist else None
+        image : image file
+        file_name : Father, Mother, Baby
     Return:
         generated image url
     """
-    if image2:
-        father_image_name = common_uuid + "Father.png"
-        mother_image_name = common_uuid + "Mother.png"
+    image_name = common_uuid + file_name + ".png"
+    save_path = file_name + "/" + image_name
 
-        s3.upload_fileobj(
-            image1.file, "12war", "man/"+father_image_name, 
-            ExtraArgs={"ContentType": "image/png", "ACL": "public-read"}
-        )
-        s3.upload_fileobj(
-            image2.file, "12war", "woman/"+mother_image_name, 
-            ExtraArgs={"ContentType": "image/png", "ACL": "public-read"}
-        )
+    s3.upload_fileobj(
+        image.file, "12war", save_path, 
+        ExtraArgs={"ContentType": "image/png", "ACL": "public-read"}
+    )
         
-        father_url = s3_get_image_url(s3, "man/"+father_image_name)
-        mother_url = s3_get_image_url(s3, "woman/"+mother_image_name)
+    image_url = s3_get_image_url(s3, save_path)
         
-        return father_url, mother_url
-    
-    else:
-        baby_image_name = common_uuid + "Baby.png"
-        
-        s3.upload_fileobj(
-            image1.file, "12war", "baby/"+baby_image_name, 
-            ExtraArgs={"ContentType": "image/png", "ACL": "public-read"}
-        )
-
-        baby_url = s3_get_image_url(s3, "baby/"+baby_image_name)
-
-        return baby_url
+    return image_url
 
 
