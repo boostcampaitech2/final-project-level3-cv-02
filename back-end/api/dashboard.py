3f921e428345e11b51813a3972b3e71d18e851d7
swkim-sm <swit1712155@sookmyung.ac.kr>
2021-12-19 11:47:21 +0000

    add: Temp dashboard

--- back-end/api/dashboard.py
+++ back-end/api/dashboard.py
@@ -25,20 +25,51 @@ from model.database import SessionLocal, engine
 router = APIRouter()
 load_dotenv(dotenv_path = ".env")
 
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        return db
+    finally:
+        db.close()
 
-@router.get("/dashboard")
-def get_user_info():
+
+@router.get("/dashboard/inflow")
+def get_user_inflow():
     """
     성별과 나이에 따른 유입량을 반환합니다.
     Return:
         list : 남성, 여성, 전체 이용자의 나이대 별 유입 횟수
     """
-    entrance_by_gender = [
+    inflow_by_gender = [
         {"gender": "Male", "count": [1, 1, 1, 1, 1]},
         {"gender": "Female", "count": [2, 2, 2, 2, 2]},
         {"gender": "Total", "count": [3, 3, 3, 3, 3]}
     ]
 
-    return entrance_by_gender
+    return inflow_by_gender
 
+@router.get("/dashboard/bounce_rate")
+def get_bounce_rate():
+    """
+    결과물이 나올 때까지 기다린 사람과 이탈한 사람의 수를 반환합니다.
+    Return:
+        list : 기다린 사람과 이탈한 사람의 수
+    """
+    stay = 0
+    no_stay = 0
 
+    db = get_db()
+
+    users = crud.get_inference_results(db, skip=0, limit=100)
+
+    for user in users:
+        print(user)
+        if user.complete:
+            stay += 1
+        else:
+            no_stay += 1
+
+    bounce_rate = [{"stay":stay, "no_stay":no_stay}] 
+
+    return bounce_rate
