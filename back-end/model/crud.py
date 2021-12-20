74321e38dde3cb78371b2c826980594f00973b4f
JODONG2 <whgusehd96@gmail.com>
2021-12-20 08:49:48 +0000

    feat: save baby url

--- back-end/model/crud.py
+++ back-end/model/crud.py
@@ -1,6 +1,16 @@
 from sqlalchemy.orm import Session
 from . import models, schemas
 from fastapi import Depends
+from sqlalchemy import update
+from sqlalchemy.sql import func
+
+# stmt = (
+#     update(user_table).
+#     where(user_table.c.id == 5).
+#     values(name='user #5')
+# )
+
+
 
 def get_inference_results(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.InferenceResult).offset(skip).limit(limit).all()
@@ -11,8 +21,28 @@ def create_inference_result(db: Session, inference_result: schemas.InferenceResu
         father_url = inference_result['father_url'],
         mother_url = inference_result['mother_url'],
         baby_url = inference_result['baby_url'],
-        comment = inference_result['comment']
+        comment = inference_result['comment'],
+        complete = inference_result['complete'] # True
     )
-    db.add(db_result)   
+    db.add(db_result)
     db.commit()
+    print("in 57@@@@" *30)
     return db_result
+
+def update_inference_result (db:Session, uuid: str, baby_url:str ):#, comment:str)  # inference_result:schemas.InferenceResultCreate):
+    
+    db_update = db.query(models.InferenceResult).filter(models.InferenceResult.id == uuid).one()
+
+    print (db_update)
+
+    if not db_update.complete: #실패 
+        db_update.closed_at = func.now() # baby_url =  None 
+        db_update.baby_url = None 
+    else:  #성공 
+        db_update.closed_at = func.now()
+        db_update.baby_url = baby_url 
+
+    db.commit()
+
+    return db_update
+
